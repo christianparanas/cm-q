ed66bc6eb0272ae1e60e0ee82ea310e81391fb3e
Christian Paranas <christiannparanas@gmail.com>
2021-04-26 19:16:19 +0800

    nav

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 import './style.scss'
 
 document.querySelector('#app').innerHTML = `
+	<div class="nav">
+		<div class="navv">ACT 2</div>
+		<div class="navv">ACT 3</div>
+		<div class="navv">ACT 4</div>
+		<div class="navv">ACT 5</div>
+		<div class="navv">ACT 6</div>
+		<div class="navv">ACT 7</div>
+		<div class="navv">ACT 8</div>
+	</div>
   <div class="container">
     <div class="box">
-
         <div class="title">
             <span class="block"></span>
             <h1>Christian Paranas<span></span></h1>
@@ -11,7 +19,7 @@ document.querySelector('#app').innerHTML = `
 
         <div class="role">
             <div class="block"></div>
-            <p>UI Dev designer</p>
+            <p>Student</p>
         </div>
 
     </div>
