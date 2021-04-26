280fc3ba1c7be02f5d971820a37593ca7e57c94c
Christian Paranas <christiannparanas@gmail.com>
2021-04-26 18:26:33 +0800

    init

---
+++ main.js
@@ -0,0 +1,20 @@
+import './style.scss'
+
+document.querySelector('#app').innerHTML = `
+  <div class="container">
+    <div class="box">
+
+        <div class="title">
+            <span class="block"></span>
+            <h1>Christian Paranas<span></span></h1>
+        </div>
+
+        <div class="role">
+            <div class="block"></div>
+            <p>UI Dev designer</p>
+        </div>
+
+    </div>
+</div>
+
+`
