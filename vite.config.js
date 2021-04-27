a958ae0fd5727e2146e5c1d593b5b1254fbad3f5
Christian Paranas <christiannparanas@gmail.com>
2021-04-27 18:38:14 +0800

    try this solution

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+const { resolve } require('path')
+
+
+module.exports = {
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html')
+				pages: resolve(__dirname, '/pages/chapter2.html')
+			}
+		}
+	}
+}
