59c7d6087473b9a1df1f60411df2d7fe078a1381
Christian Paranas <christiannparanas@gmail.com>
2021-04-27 20:24:22 +0800

    chap6

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ module.exports = {
 				page2: resolve(__dirname, '/pages/chapter3.html'),
 				page3: resolve(__dirname, '/pages/chapter4.html'),
 				page4: resolve(__dirname, '/pages/chapter5.html'),
+				page6: resolve(__dirname, '/pages/chapter6.html'),
 			}
 		}
 	}
