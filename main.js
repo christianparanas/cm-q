abe2cb9fd3cb73c653a97b7f91fe73bf28d14c46
Christian Paranas <christiannparanas@gmail.com>
2021-04-27 17:02:46 +0800

    new btns

--- main.js
+++ main.js
@@ -2,13 +2,41 @@ import './style.scss'
 
 document.querySelector('#app').innerHTML = `
 	<div class="nav">
-		<div class="navv">ACT 2</div>
-		<div class="navv">ACT 3</div>
-		<div class="navv">ACT 4</div>
-		<div class="navv">ACT 5</div>
-		<div class="navv">ACT 6</div>
-		<div class="navv">ACT 7</div>
-		<div class="navv">ACT 8</div>
+		<button class="pushable">
+		  <span class="front">
+		    ACT 2
+		  </span>
+		</button>
+		<button class="pushable">
+		  <span class="front">
+		    ACT 3
+		  </span>
+		</button>
+		<button class="pushable">
+		  <span class="front">
+		    ACT 4
+		  </span>
+		</button>
+		<button class="pushable">
+		  <span class="front">
+		    ACT 5
+		  </span>
+		</button>
+		<button class="pushable">
+		  <span class="front">
+		    ACT 6
+		  </span>
+		</button>
+		<button class="pushable">
+		  <span class="front">
+		    ACT 7
+		  </span>
+		</button>
+		<button class="pushable">
+		  <span class="front">
+		    ACT 8
+		  </span>
+		</button>
 	</div>
   <div class="container">
     <div class="box">
